9a1e1d21da700a43e90b753d578f6af4ab8937c3
rwang2022 <ryanwang601@gmail.com>
2023-05-20 16:27:18 -0400

    xd

--- Stack/22_generate-parantheses.py
+++ Stack/22_generate-parantheses.py
@@ -2,6 +2,12 @@ def generateParenthesis(n: int):
     """
     every valid string looks like 
     ( + leftStr + ) + rightStr
+    
+    leftStr is the list of valid i pairs, as i varies from 0 to n-1 (leftCount = i)
+    rightStr is the list of valid n-i-1 pairs
+
+    iterate through these two lists to find each validStr for each i.
+    vary i from 0 through n-1 for every validStr, for validList
     """
 
     if (n == 0):
